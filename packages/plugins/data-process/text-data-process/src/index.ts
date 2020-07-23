c6102cfc33a7f5ab33295ae7d3119187bfe7016e
WenheLI <wl1508@nyu.edu>
2020-07-23 11:45:43 +0800

    feat: add text data process plugin

--- packages/plugins/data-process/text-data-process/src/index.ts
+++ packages/plugins/data-process/text-data-process/src/index.ts
@@ -1,10 +1,23 @@
 import { DataProcessType, ArgsType, Sample, Metadata } from '@pipcook/pipcook-core'
+import { MakeWordsSet } from './scripts';
+
+const boa = require('@pipcook/boa');
+const jieba = boa.import('jieba');
 
 const textDataProcess: DataProcessType = async (data: Sample, metadata: Metadata, args: ArgsType): Promise<void> => {
   const {
     stopwordsPath = ""
   } = args;
-  
+
+  let stopwordsSet: Set<string>;
+
+  if (stopwordsPath) {
+    stopwordsSet = await MakeWordsSet(stopwordsPath);
+  }
+
+  const words: string[] = jieba.cut(data.data, boa.kwargs({cut_all: false}));
+  if (stopwordsPath) data.data = words.filter((word) => !stopwordsSet.has(word));
+  else data.data = words;
 }
 
 export default textDataProcess;
