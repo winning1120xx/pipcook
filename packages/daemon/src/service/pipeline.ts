c7742f4db50edf0804bc3c3617928a9b7c24372b
queyue.crk <queyue.crk@alibaba-inc.com>
2020-10-10 14:22:12 +0800

    Backward compatible

--- packages/daemon/src/service/pipeline.ts
+++ packages/daemon/src/service/pipeline.ts
@@ -260,7 +260,7 @@ export class PipelineService {
         job.dataset = JSON.stringify(datasetVal.metadata);
       }
       job.evaluateMap = JSON.stringify(result);
-      job.evaluatePass = result?.evaluateResult?.pass;
+      job.evaluatePass = result?.evaluateResult?.pass || result?.pass;
       job.endTime = Date.now();
       job.status = PipelineStatus.SUCCESS;
 
